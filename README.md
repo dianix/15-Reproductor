c1137d4659e53962c053d0967e502863b0ea8453
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-03-27 23:56:29 -0600

    Actualizada la barra de progreso, reloj de progreso y ajuste de estilos

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# **** README EN PROGRESO ****
+# *** DESCRIPCIÓN DEL EJERCICIO REPRODUCTOR DE AUDIO ***
+
+1. Documento de html con una sección principal que contiene:
+   * Enlace al archivo de audio.
+   * barra de reproducción.
+   * Botones para control de audio.   
+
+2. Documento de javascript que contiene las funciones encargadas de:
+   * Ejecutar las acciones de los botones.
+   * Mover la barra de reproducción de acuerdo al tiempo transcurrido.
+   * Ajustar el formato del tiempo reproducido y enviarlo al elemento correspondiente del html.
+
+3. Documento estilos.css para configurar el aspecto de algunos elementos de la página.
--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,9 @@
 //alert("Holaaaaaa");
+
+// VARIABLE GLOBAL QUE GUARDA EL AUDIO
 var elementoAudio = (document.getElementById("archivoAudio"));
 
+// *** FUNCIONES PARA BOTONES ***
 function reproducir(){
     elementoAudio.play();
 }
@@ -11,19 +14,6 @@ function pausar(){
 
 function parar(){
     elementoAudio.load();
-}
-
-function progreso(){
-    var duracionAudio = elementoAudio.duration;
-    var barra = document.getElementById("barraProgreso");
-     barra.value = elementoAudio.currentTime;
-    barra.max = duracionAudio;
-    tiempo();
-}
-
-function tiempo(){
-    var tiempo = document.getElementById("tiempo");
-    tiempo.innerHTML = elementoAudio.currentTime;
 }
 
 function subirVolumen(){
@@ -41,3 +31,44 @@ function avanzar(){
 function retroceder(){
     elementoAudio.currentTime -= 2;
 }
+
+// *** PARA MOVER BARRA DE PROGRESO Y ENVIAR AL RELOJ EL TIEMPO TRANSCURRIDO ***
+elementoAudio.ontimeupdate = function(){
+    var barraProgreso = document.getElementById("barraProgreso");
+    barraProgreso.value = elementoAudio.currentTime;
+    
+    var tiempoActual = elementoAudio.currentTime;
+    var contadorTiempo = formatoReloj(tiempoActual);
+    document.getElementById("tiempo").innerHTML = contadorTiempo;
+}
+
+// *** PARA DAR FORMATO AL TIEMPO DE REPRODUCCIÓN *** 
+function formatoReloj(tiempo){
+    var segundos = Math.floor(tiempo%60);
+    var minutos = Math.floor(tiempo/60);
+    var reloj;
+    if(segundos < 10){
+        segundos = "0" + segundos;
+    }
+    if(minutos < 10){
+        minutos = "0" + minutos;
+    }
+    reloj = minutos + ":" + segundos;
+    return reloj;
+}
+
+/* 
+function tiempo(){
+    var tiempo = document.getElementById("tiempo");
+    tiempo.innerHTML = elementoAudio.currentTime;
+}*/
+
+
+/* no exitosa
+function progreso(){
+    var duracionAudio = elementoAudio.duration;
+    var barra = document.getElementById("barraProgreso");
+    barra.value = elementoAudio.currentTime;
+    barra.max = duracionAudio;
+    tiempo();
+} */
